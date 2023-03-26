4ec9a591611f6810a349a6b6b655a18056efe225
Pavel9889 <tymandru@gmail.com>
2023-03-26 21:14:51 +0300

    refactoring code, second lesson

--- app/src/main/java/com/example/studyappjava/MainActivity.java
+++ app/src/main/java/com/example/studyappjava/MainActivity.java
@@ -11,50 +11,102 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
-    EditText Num1, Num2, Operation;
-    TextView ResultText;
-    Button button;
+   private EditText etNum1, etNum2, etOperation;
+    private TextView tvResultText;
+    private Button btnCalculateResult;
+
+    private Toast toastError;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Num1 = findViewById(R.id.editText1);
-        Num2 = findViewById(R.id.editText2);
-        Operation = findViewById(R.id.editTextOperation);
-        ResultText = findViewById(R.id.Result);
-        button = findViewById(R.id.button);
-        button.setOnClickListener(this);
+        etNum1 = findViewById(R.id.num1_editText);
+        etNum2 = findViewById(R.id.num2_edit_text);
+        etOperation = findViewById(R.id.operation_edit_text);
+        tvResultText = findViewById(R.id.result_text_view);
+        btnCalculateResult = findViewById(R.id.result_button);
+        btnCalculateResult.setOnClickListener(this);
     }
 
     @Override
     public void onClick(View v) {
         float num1, num2, result = 0;
+        boolean correctOperation = true;
+        String operation = "";
+        try {
 
-        num1 = Float.parseFloat(Num1.getText().toString());
-        num2 = Float.parseFloat(Num2.getText().toString());
-        String task = Operation.getText().toString();
+
+            num1 = Float.parseFloat(etNum1.getText().toString());
+            num2 = Float.parseFloat(etNum2.getText().toString());
+            operation = etOperation.getText().toString();
 
 
-        switch (task) {
-            case "+":
-                result = num1 + num2;
-                break;
-            case "-":
-                result = num1 - num2;
-                break;
-            case "/":
-                result = num1 / num2;
-                break;
-            case "*":
-                result = num1 * num2;
-                break;
-            default:
-                break;
+            switch (operation) {
+                case "+":
+                    result = num1 + num2;
+                    break;
+                case "-":
+                    result = num1 - num2;
+                    break;
+                case "/":
+                    if (num2==0) throw new ArithmeticException();
+                    result = num1 / num2;
+                    break;
+                case "*":
+                    result = num1 * num2;
+                    break;
+                default: correctOperation = false;
+                    break;
+            }
+        }catch (ArithmeticException e ){
+            int duration =Toast.LENGTH_SHORT;
+            if (toastError != null) {
+                toastError.cancel();
+            }
+            toastError = Toast.makeText(this, R.string.divide_zero,duration);
+            toastError.show();
+            return;
+            } catch (NullPointerException e){
+            int duration =Toast.LENGTH_SHORT;
+            if (toastError != null) {
+                toastError.cancel();
+            }
+            toastError = Toast.makeText(this, R.string.null_data,duration);
+            toastError.show();
+            return;
+        }   catch (NumberFormatException e){
+            int duration =Toast.LENGTH_SHORT;
+            if (toastError != null) {
+                toastError.cancel();
+            }
+            toastError = Toast.makeText(this, R.string.wrong_format,duration);
+            toastError.show();
+            return;
+
         }
-        ResultText.setText("Результат:"+ " " + num1 + " " + task + " " + num2 + "=" + result);
+
+        if (correctOperation == true) {
+            tvResultText.setText(R.string.result + " " + num1 + " " + operation + " " + num2 + "=" + result);
+        } else  {
+            int duration =Toast.LENGTH_SHORT;
+            if (toastError != null) {
+                toastError.cancel();
+            }
+            toastError = Toast.makeText(this, R.string.wrong_operation,duration);
+            toastError.show();
+            return;
+
+        }
+        }
+
+
+
+
+
+
 
     }
 
 
-}
+
